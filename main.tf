25c3b9690a0f9a99503c467c7e8c21513ab8ace4
Guru <150461413+Guru-96@users.noreply.github.com>
2024-04-08 11:07:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ops" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "guru"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "guru3399"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}        
